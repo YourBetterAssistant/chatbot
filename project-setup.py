6e27cd80da5bcc38cd63bdadaa01f5a02cf6f155
NotTimIsReal <alastairrintumathew@gmail.com>
2022-03-29 20:18:03 +1100

    testing for other projec

--- project-setup.py
+++ project-setup.py
@@ -1,4 +1,5 @@
 import subprocess
-cmd=subprocess.run(["pip3", "install", "-r", "requirements.txt"])
+import subprocess
+cmd=subprocess.run(["pip3", "install", "-r", "requirements.txt", ">>", "/dev/null"])
 status="".join(map(chr,cmd))
 print("setup.project.status.sucess")
