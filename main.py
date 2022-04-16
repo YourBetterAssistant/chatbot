5dd5ce1d41fa4d49f97606a00f61b5dd2d8b2263
NotTimIsReal <alastairrintumathew@gmail.com>
2022-04-16 20:57:00 +1000

    still fixing

--- main.py
+++ main.py
@@ -32,6 +32,6 @@ app = FastAPI()
 def show_Home(message: str = "Hello"):
     return {"message": f"{bot.get_response(message)}"}
 @app.post("/train")
-def train(toBeTrained: "list[str]"):
+def train_Bot(toBeTrained: "list[str]"):
     bot.train(toBeTrained)
     return "Training"
